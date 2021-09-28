d63a9be6f513ee6bde35502cc5482c767ff43e65
RickRusso <sahlofolina2211@gmail.com>
2021-09-28 15:25:09 +0300

    End of section 9

--- API/DTOs/MemberDTO.cs
+++ API/DTOs/MemberDTO.cs
@@ -8,11 +8,11 @@ namespace API.DTOs
     public class MemberDTO
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string PhotoUrl { get; set; }
-        public string KnownAs { get; set; }
         public int Age { get; set; }
-        public DateTime RegistrationDate { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
         public string Gender { get; set; }
         public string Introduction { get; set; }
